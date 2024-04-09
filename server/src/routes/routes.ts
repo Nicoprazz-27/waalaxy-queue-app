518128ea989b2c077b5cade852fb6f0a9178eb19
Nicoprazz <nicolas.sirjent@groupe-esigelec.org>
2024-04-09 17:30:10 +0200

    🐛Use the getQueueActionsController instead the getActionsController

--- server/src/routes/routes.ts
+++ server/src/routes/routes.ts
@@ -1,11 +1,12 @@
 import express, { Router } from 'express';
 import getActionsController from '../controllers/actions-controller/get-actions-controller';
+import getQueueActionsController from '../controllers/queue-actions-controller/get-queue-actions-controller';
 
 
 const router: Router = express.Router();
 
 router.get('/actions', getActionsController);
 
-router.get('/queue-actions', getActionsController);
+router.get('/queue-actions', getQueueActionsController);
 
 export default router;
